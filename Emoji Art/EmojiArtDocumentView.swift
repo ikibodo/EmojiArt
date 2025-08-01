610086e0b75a36423d91d08f56b90d91b1a5fe06
kibodo <kibodo@ya.ru>
2025-08-01 13:10:38 +0200

    feat: add panGesture

--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -31,10 +31,12 @@ struct EmojiArtDocumentView: View {
             ZStack {
                 Color.white
                 documentContents(in: geometry)
-                    .scaleEffect(zoom * gestureZoom)
-                    .offset(pan)
+                    .scaleEffect(zoom * gestureZoom) // см CGOffset в Extensions
+                    .offset(pan + gesturePan) // см CGOffset в Extensions
             }
-            .gesture(zoomGesture)
+            .gesture(panGesture.simultaneously(with: zoomGesture))
+//            .gesture(zoomGesture)
+//            .gesture(panGesture)
             .dropDestination(for: Sturldata.self) { sturldatas, location in
                 return drop(sturldatas, at: location, in: geometry)
             }
@@ -45,6 +47,7 @@ struct EmojiArtDocumentView: View {
     @State private var pan: CGOffset = .zero // см CGOffset в Extensions
     
     @GestureState private var gestureZoom: CGFloat = 1
+    @GestureState private var gesturePan: CGOffset = .zero
     
     private var zoomGesture: some Gesture {
         MagnificationGesture()
@@ -53,6 +56,16 @@ struct EmojiArtDocumentView: View {
             }
             .onEnded { endingPinchScale in
                 zoom *= endingPinchScale
+            }
+    }
+    
+    private var panGesture: some Gesture {
+        DragGesture()
+            .updating( $gesturePan) { value, gesturePan, _ in
+                gesturePan = value.translation
+            }
+            .onEnded { value in
+                pan += value.translation // см CGOffset += в Extensions
             }
     }
     
