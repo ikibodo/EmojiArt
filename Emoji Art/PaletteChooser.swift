f718dab407bdc467a1ca17ff9b1375cb794e6138
kibodo <kibodo@ya.ru>
2025-08-07 20:01:20 +0200

    feat: add PaletteChooser as a second View

---
+++ Emoji Art/PaletteChooser.swift
@@ -0,0 +1,57 @@
+//
+//  PaletteChooser.swift
+//  Emoji Art
+//
+//  Created by N L on 7. 8. 2025..
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    @EnvironmentObject var store: PaletteStore // получи что-то типа PaletteStore что было внедрено в меня (и это что-то только одно)
+    
+    var body: some View {
+        HStack {
+            chooser
+            view(for: store.palettes[store.cursorIndex])
+        }
+    }
+    
+    var chooser: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "paintpalette")
+        }
+    }
+    
+    func view(for palette: Palette) -> some View {
+        HStack {
+            Text(palette.name)
+            ScrollingEmojis(palette.emojis)
+        }
+    }
+}
+
+struct ScrollingEmojis: View {
+    let emojis: [String]
+    init(_ emojis: String) {
+        self.emojis = emojis.uniqued.map(String.init)
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .draggable(emoji)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PaletteChooser()
+        .environmentObject(PaletteStore(named: "Preview"))
+}
