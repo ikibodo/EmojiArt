6d28ad00381760651a70d5e43cca334d6d02a660
kibodo <kibodo@ya.ru>
2025-08-07 20:16:59 +0200

    feat: add  extension for Transition and Button

--- Emoji Art/Extensions.swift
+++ Emoji Art/Extensions.swift
@@ -36,3 +36,38 @@ extension String {
         }
     }
 }
+
+extension AnyTransition {
+    static let rollUp: AnyTransition = asymmetric(insertion: .move(edge: .bottom), removal: .move (edge: .top))
+    static let rollDown: AnyTransition = .asymmetric(insertion: .move(edge: .top), removal: .move(edge: .bottom) )
+}
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    var role: ButtonRole?
+    let action: () -> Void
+    
+    init(_ title: String? = nil, systemImage: String? = nil, role: ButtonRole? = nil, action: @escaping () -> Void) {
+        self.title = title
+        self.systemImage = systemImage
+        self.role = role
+        self.action = action
+    }
+    
+    var body: some View {
+        Button (role: role) {
+            withAnimation {
+                action ()
+            }
+        } label: {
+            if let title, let systemImage {
+                Label(title, systemImage: systemImage)
+            } else if let title {
+                Text (title)
+            } else if let systemImage {
+                Image (systemName: systemImage)
+            }
+        }
+    }
+}
