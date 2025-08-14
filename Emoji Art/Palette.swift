71643e02e6e54c72d27e1a66e9dffd5f0e2e766b
kibodo <kibodo@ya.ru>
2025-08-14 13:07:15 +0200

    feat:  add navigation for PaletteList

--- Emoji Art/Palette.swift
+++ Emoji Art/Palette.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 // Model_Two
-struct Palette: Identifiable, Codable {
+struct Palette: Identifiable, Codable, Hashable {
     var name: String
     var emojis: String
     var id = UUID()
--- Emoji Art/PaletteList.swift
+++ Emoji Art/PaletteList.swift
@@ -11,11 +11,35 @@ struct PaletteList: View {
     @EnvironmentObject var store: PaletteStore
     
     var body: some View {
-        VStack {
-            ForEach(store.palettes) { palette in
-                Text(palette.name)
+        NavigationStack { // контейнер для навигации всего что внутри него
+            List(store.palettes) { palette in // VStack -> List  ( как и Form часто используется) имеет свой встроенный ForEach
+                NavigationLink(value: palette) { // что будет нажиматься и с чем ассоциировано
+                    Text(palette.name)
+                }
+            }
+            .navigationDestination(for: Palette.self) { palette in
+                PaletteView(palette: palette)
             }
         }
+    }
+}
+
+struct PaletteView: View {
+    let palette: Palette
+    
+    var body: some View {
+        VStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(palette.emojis.uniqued.map(String.init), id: \.self) { emoji in
+                    NavigationLink(value: emoji) {
+                        Text(emoji)
+                    }
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .font(.largeTitle)
     }
 }
 
