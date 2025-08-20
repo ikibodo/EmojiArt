ea5bbbbf0b09c3c58208db4f8d351f3d372b0beb
kibodo <kibodo@ya.ru>
2025-08-20 16:40:55 +0200

    feat:  add onTapGesture  for zoomToFit

--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -41,6 +41,9 @@ struct EmojiArtDocumentView: View {
                     .offset(pan + gesturePan)
             }
             .gesture(panGesture.simultaneously(with: zoomGesture))
+            .onTapGesture(count: 2) {
+                zoomToFit(document.bbox, in: geometry)
+            }
             .dropDestination(for: Sturldata.self) { sturldatas, location in
                 return drop(sturldatas, at: location, in: geometry)
             }
